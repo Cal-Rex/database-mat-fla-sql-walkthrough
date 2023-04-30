e06b5edd78380c8a2af0968f085b163465aa5d99
Cal-Rex <andy.gibb@healthcareskills.com>
2023-04-30 09:03:41 +0000

    add for loop to iterate db entries on categories page, add function into routes to achieve this

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -10,8 +10,27 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    # when page is loaded, the variable below queries the Category table for all entries, and then orders them by name
+    # wrapping the variable value in a list( ) tag orders the data in the form of a list/array
+    categories = list(Category.query.order_by(Category.category_name).all())
+    # categories=categories explained:
+    # categories #1 = is the variable name that refers to categories.html
+    # categories #2 = is the variable defined inside this function (categories = list(...) )
+    return render_template("categories.html", categories=categories)
 
-@app.route("/add_category", methods=["GET","POST"])
+
+# the first method applied here is regarded as the default method
+# (that would be GET, in this instance)
+# however, if the page is loaded with a POST action...
+# (like when a form is submitted and results in it loading)
+# the if statement will run instead
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        # creates new instance of the "Category" model
+        # imported at the top of the file
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
