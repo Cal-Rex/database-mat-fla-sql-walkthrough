281b373d43cbfa18256f21c66b4e2693c5e7e88d
Cal-Rex <andy.gibb@healthcareskills.com>
2023-05-01 08:52:31 +0000

    add modal to delete categories function for data defense

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -10,3 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
     //     $('.sidenav').sidenav();
     // });
 // ---------------------------------------------------------
+
+// ---------------------------------------------------------
+// initialisation for "delete" modals
+document.addEventListener('DOMContentLoaded', function() {
+  var modal = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(modal);
+});
+
+  //   same code as above, but for if JQuery being used
+  // $(document).ready(function(){
+  //   $('.modal').modal();
+  // });
